aeea503535152c049e38f17c8e2d34750c72b1d9
unknown <winden@gmail.com>
2014-02-01 15:32:55 +0800

    added getproxy used to get proxy from a website. added mkproxyt used to make proxy from tab seperated proxy. by FZD

---
+++ getproxy.py
@@ -0,0 +1,21 @@
+import urllib.request
+
+proxy_url = 'http://www.kuaidaili.com/proxylist/'
+user_agent = "Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25"
+
+def BuildReq(url):
+    req = urllib.request.Request(url)
+    req.add_header('User-Agent',user_agent)
+    return req
+
+if __name__ == '__main__':
+    fp = open('proxy.html','w',encoding='utf-8')
+    for i in range(10):
+        n = i+1
+        url = proxy_url + str(n)
+        req = BuildReq(url)
+        data = urllib.request.urlopen(req).read()
+        fp.write(data.decode('utf-8'))
+
+
+
---
+++ mkproxyt.py
@@ -0,0 +1,23 @@
+import re
+
+fp = open('proxy.html','r',encoding='utf-8')
+proxyfile = open('proxy.txt','a')
+
+lines = fp.readlines()
+port_flag = 0
+for line in lines:
+    if port_flag == 0:
+        ip = re.findall(r'\d+\.\d+\.\d+\.\d+',line)
+        if ip:
+            ip = ip[0]
+            port_flag = 1
+    else: 
+        port_flag = 0
+        port = re.findall(r'\d+',line)
+        if port:
+            port = port[0]
+            proxy = ip+':'+port
+            proxyfile.write(proxy+'\n')
+        
+
+
